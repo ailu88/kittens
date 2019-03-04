353d503c5ff9e45a224470b9a971eceea3323e44
berenice bourgon <berenice.bourgon@gmail.com>
2019-03-04 15:47:19 +0100

    creation of User model with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # We are adding Devise to our app:
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # For Devise, here is the mailer: 
+  config.action_mailer.default_url_options = { :host => 'kittens-gang-development.herokuapp.com' }
 end
