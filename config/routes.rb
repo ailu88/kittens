ac811131c18a3934fa6f23c65cde2836d9a7d1d8
berenice bourgon <berenice.bourgon@gmail.com>
2019-03-04 16:47:41 +0100

    change of routes file - solving the conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-  get 'home/index'
-=======
-
 	resources :items
 	root "items#index"
->>>>>>> development
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
